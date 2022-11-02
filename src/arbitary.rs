b577b6228fb04f2212335cb98bf87f580daddffe
penguin-number-123 <mumuji1723@gmail.com>
2022-11-02 15:47:44 +0800

    Update arbitary.rs

--- src/arbitary.rs
+++ src/arbitary.rs
@@ -1,7 +1,7 @@
 pub use self::BigFloat;
-mod arbitary;
+
 use std::cmp;
-[#derive(eq)]
+
 pub struct BigFloat{
     sign: i32,
     front: Vec<i32>,
@@ -14,14 +14,18 @@ pub struct BigFloat{
         let p = cmp::max(front.len(),back.len()) as i64;
         BigFloat{sign,front,back,prec:p}
     }
-    fn fix(BigFloat:a,BigFloat:b)->(BigFloat,BigFloat){
+    fn fix(a:BigFloat,b:BigFloat) -> (BigFloat,BigFloat){
         let mut ma:BigFloat;
-        let mut mb:BigFloat
+        let mut mb:BigFloat;
+        let mut bk= vec![0;(a.prec-b.prec) as usize];
         if a.prec>b.prec{
-            mb.front= vec![0;a.prec-b.prec].append(&mut b.front[0]);
-            mb.back = mb.back.append(&mut vec![0;a.prec-b.prec]);
+            bk.append(vec![&mut b.front[0]]);
+            mb.front = bk;
+            mb.back.append(&mut vec![0;(a.prec-b.prec) as usize]);
         }
+        (ma,mb)
     }
+    fn eq
     // def fix(self,other):
         // if self.prec > other.prec:
             // other.front= [0]*(self.prec-other.prec) + self.front
